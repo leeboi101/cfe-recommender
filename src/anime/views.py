551d412e0f6f9243593724029e33ae1a434847e2
leeboi101 <148595347+leeboi101@users.noreply.github.com>
2024-03-01 19:37:13 +0000

    my_ratings is working

--- src/anime/views.py
+++ src/anime/views.py
@@ -9,6 +9,17 @@ class AnimeListView(generic.ListView):
     # context -> object_list
     queryset = Anime.objects.all().order_by('-rating_avg')
 
+    def get_context_data(self,*args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        request = self.request
+        user = request.user
+        if user.is_authenticated:
+            object_list = context['object_list']
+            object_ids = [x.id for x in object_list]
+            qs = user.rating_set.filter(active=True, object_id__in=object_ids)
+            context['my_ratings'] = {"16206": 10}
+        return context
+
 anime_list_view = AnimeListView.as_view()
 
 class AnimeDetailView(generic.DetailView):
