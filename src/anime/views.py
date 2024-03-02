afbde685730605f27db3a3d9492568e1254cfaaa
leeboi101 <148595347+leeboi101@users.noreply.github.com>
2024-03-02 15:59:43 +0000

    first version of creating the storing process of the ratings

--- src/anime/views.py
+++ src/anime/views.py
@@ -1,18 +1,44 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.views import generic
 
 from .models import Anime
 
+
+SORTING_CHOICES = {
+    "popular": "-rating_avg",
+    "unpopular": "rating_avg",
+    "recent": "-start_date",
+    "old": "start_date"
+}
+
 class AnimeListView(generic.ListView):
-    template_name = 'anime/list.html'
+    template_name = 'anime/list-view.html'
     paginate_by = 100
     # context -> object_list
-    queryset = Anime.objects.all().order_by('-rating_avg')
+
+    def get_queryset(self):
+        request = self.request
+        default_sort = request.session.get('anime_sort_order') or 'rating_avg'
+        qs = Anime.objects.all().order_by(default_sort)
+        request = self.request
+        sort = request.GET.get('sort')
+        if sort is not None:
+            request.session['anime_sort_order'] = sort
+            qs = qs.order_by(sort)
+        return qs
+
+    def get_template_names(self):
+        request = self.request
+        if request.htmx:
+            return ['anime/snippet/list.html']
+        return ['anime/list-view.html']
 
     def get_context_data(self,*args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         request = self.request
         user = request.user
+        context['sorting_choices'] = SORTING_CHOICES
         if user.is_authenticated:
             object_list = context['object_list']
             object_ids = [x.id for x in object_list]
--- src/ratings/views.py
+++ src/ratings/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def rate_movie_view(request):
+    if not request.htmx:
+        return HttpResponse("Not Allowed", status=404)
+    object_id = request.POST.get('object_id')
+    rating_value = request.POST.get('')
+    rating_value = request.POST.get('')
+    user = request.user
+    message = "You must <a href='/accounts/login/'>login</a> to Rate this."
+    if user.is_authenticated:
+        pass
