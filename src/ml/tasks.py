0e25c92b730d5e306b095cb13f873e1c813594ad
leeboi101 <148595347+leeboi101@users.noreply.github.com>
2024-03-09 16:03:34 +0000

    code for tasks updated with recently_suggested as well as models for suggestions app updated

--- src/ml/tasks.py
+++ src/ml/tasks.py
@@ -10,77 +10,41 @@ from . import utils as ml_utils
 def train_surprise_model_task():
     ml_utils.train_surprise_model()
 
-
-# @shared_task
-# def batch_users_prediction_task(users_ids=None, start_page=0, offset=250, max_pages=1000):
-#     model = ml_utils.load_model()
-#     suggestion = apps.get_model('suggestions', 'Suggestion')
-#     ctype = ContentType.objects.get(app_label='anime', model='anime')
-#     end_page = start_page + offset
-#     if users_ids is None:
-#         users_ids = profiles_utils.get_recent_users()
-#     anime_ids = Anime.objects.all().popular().values_list('id', flat=True)[start_page:end_page]
-#     new_suggestion = []
-#     for anime_id in anime_ids:
-#         for u in users_ids:
-#             pred = model.predict(uid=u, iid=anime_id).est
-#             data = {
-#                 'user_id':u,
-#                 'object_id': anime_id,
-#                 'value': pred,
-#                 'content_type': ctype,
-
-
-#             }
-#             new_suggestion.append(
-#                 suggestion(**data)
-#             )
-#     suggestion.objects.bulk_create(new_suggestion, ignore_conflicts=True)
-#     if end_page < max_pages:
-#         return batch_users_prediction_task(start_page=end_page-1)
-
-
-# 
 @shared_task
-def batch_users_prediction_task(users_ids=None, start_page=0, offset=250, max_pages=1000, batch_size=500):
+def batch_users_prediction_task(users_ids=None, start_page=0, offset=25, max_pages=1000):
     model = ml_utils.load_model()
     Suggestion = apps.get_model('suggestions', 'Suggestion')
     ctype = ContentType.objects.get(app_label='anime', model='anime')
-    
+    end_page = start_page + offset
+
     if users_ids is None:
         users_ids = profiles_utils.get_recent_users()
+    anime_ids = Anime.objects.all().popular().values_list('id', flat=True)[start_page:end_page]
+    recently_suggested = Suggestion.objects.get_recently_suggested(anime_ids,users_ids)
+    new_suggestion = []
 
-    total_anime_ids = Anime.objects.all().popular().count()
-    max_pages = min(max_pages, (total_anime_ids // offset) + 1)
+    for anime_id in anime_ids:
+        users_done = recently_suggested.get(f"(anime_uid)") or []
 
-    for page_num in range(start_page, max_pages):
-        start_index = page_num * offset
-        end_index = start_index + offset
-        anime_ids = Anime.objects.all().popular().values_list('id', flat=True)[start_index:end_index]
+        for u in users_ids:
+            if u in users_done:
+                    print(anime_id, 'is done for', u, 'user')
+                    continue
+            pred = model.predict(uid=u, iid=anime_id).est
+            data = {
+                'user_id':u,
+                'object_id': anime_id,
+                'value': pred,
+                'content_type': ctype,
 
-        new_suggestions = []
-        for anime_id in anime_ids:
-            for user_id in users_ids:
-                pred = model.predict(uid=user_id, iid=anime_id).est
-                # Check if a suggestion for this user and anime already exists
-                exists = Suggestion.objects.filter(user_id=user_id, object_id=anime_id).exists()
-                if not exists:
-                    data = {
-                        'user_id': user_id,
-                        'object_id': anime_id,
-                        'value': pred,
-                        'content_type': ctype,
-                    }
-                    new_suggestions.append(Suggestion(**data))
 
-                # Insert in batches to avoid excessive memory usage
-                if len(new_suggestions) >= batch_size:
-                    Suggestion.objects.bulk_create(new_suggestions, ignore_conflicts=True)
-                    new_suggestions = []
-
-        # Insert any remaining suggestions
-        if new_suggestions:
-            Suggestion.objects.bulk_create(new_suggestions, ignore_conflicts=True)
+            }
+            new_suggestion.append(
+                Suggestion(**data)
+            )
+    Suggestion.objects.bulk_create(new_suggestion, ignore_conflicts=True)
+    if end_page < max_pages:
+        return batch_users_prediction_task(start_page=end_page-1)
 
 
 def batch_single_user_prediction_task(user_id=1, start_page=0, offset=250, max_pages=1000):
--- src/suggestions/models.py
+++ src/suggestions/models.py
@@ -1,12 +1,28 @@
 
 from django.db import models
 from django.conf import settings
-
+from django.db.models import F
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 
 User = settings.AUTH_USER_MODEL #'auth.User'
+
+
+class SuggestionManager(models.Manager):
+    def get_recently_suggested(self, anime_ids=[], users_ids=[], days_ago=7):
+        data = {}
+        ctype = ContentType.objects.get(app_label='anime', model='anime')
+        dataset = self.get_queryset().filter(content_type=ctype, object_id__in=anime_ids,user_id__in=users_ids).annotate(anime_uid=F('object_id'),uid=F('user_id')).values('anime_uid','uid')
+        for d in dataset:
+            print(d)
+            anime_id = str(d.get('anime_uid'))
+            user_id = d.get('uid')
+            if anime_id in data:
+                data[anime_id].append(user_id)
+            else:
+                data[anime_id] = [user_id]
+        return data
 
 
 class Suggestion(models.Model):
@@ -21,6 +37,8 @@ class Suggestion(models.Model):
     ratin_value = models.FloatField(null=True, blank=True)
     did_rate = models.BooleanField(default=False)
     did_rate_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False,null=True, blank=True )
+    objects = SuggestionManager()
+
 
     class Meta:
         ordering = ['-timestamps']
