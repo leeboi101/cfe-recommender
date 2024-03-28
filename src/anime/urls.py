3a7d8445716512deaeac3ef7b70d435554f338bd
leeboi101 <148595347+leeboi101@users.noreply.github.com>
2024-03-28 05:58:07 +0000

    all pages now look good and styles have been completed and links work accordingly.

--- src/anime/urls.py
+++ src/anime/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 
 
 from . import views
+
+app_name = 'anime'
+
 urlpatterns = [
-    path('', views.anime_list_view),
-    path('infinite/', views.anime_infinite_rating_view),
-    path('popular/', views.anime_popular_view),
+    path('', views.anime_list_view, name='anime_list_view'),
+    path('infinite/', views.anime_infinite_rating_view, name='anime_infinite_view'),
+    path('popular/', views.anime_popular_view, name='anime_popular_view'),
     path('<int:pk>/', views.anime_detail_view)
 ]
